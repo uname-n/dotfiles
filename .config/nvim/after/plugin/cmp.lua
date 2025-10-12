2be5edc25cdfaded4f98ad4dffbd8dfc7cc93325
uname-n <git@defend.sh>
2025-10-11 22:11:03 -0700

    cmp

---
+++ .config/nvim/after/plugin/cmp.lua
@@ -0,0 +1,70 @@
+-- nvim-cmp setup
+local cmp = require("cmp")
+-- snippet support removed
+
+cmp.setup({
+  -- no snippet expansion
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      -- snippet expansion removed
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      -- snippet jump removed
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    -- snippet source removed
+  }, {
+    { name = "buffer" },
+    { name = "path" },
+  }),
+})
+
+-- Use buffer source for `/` and `?`
+cmp.setup.cmdline({ "/", "?" }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
+})
+
+-- Use cmdline & path source for ':'
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
+})
+
+-- LSP setup for lua_ls (using new vim.lsp.config API)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+vim.lsp.config["lua_ls"] = {
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      diagnostics = { globals = { "vim" } },
+      workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+      telemetry = { enable = false },
+    },
+  },
+}
+vim.lsp.enable("lua_ls")
--- .config/nvim/lua/polymer/lazy.lua
+++ .config/nvim/lua/polymer/lazy.lua
@@ -21,4 +21,10 @@ require("lazy").setup({
     { 'neoclide/coc.nvim', branch = 'release', build = ":CocInstall coc-pyright" },
     { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
     { 'laytan/cloak.nvim' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'hrsh7th/cmp-cmdline' },
+    { 'neovim/nvim-lspconfig' },
 })
